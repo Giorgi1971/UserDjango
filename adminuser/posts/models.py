604bdaf648934a148330da81ecdebf0c48c66728
gio1971 <giooorgi7@gmail.com>
2022-03-06 19:44:33 +0400

    migrations

--- adminuser/posts/models.py
+++ adminuser/posts/models.py
@@ -1,3 +1,19 @@
+from turtle import title
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+
+user = get_user_model()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=124)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(default=datetime.now())
+
+
+class twitter(models.Model):
+    follow = models.ForeignKey(User, on_delete=models.PROTECT, related_name='follow')
+    followed = models.ForeignKey(User, on_delete=models.PROTECT, related_name='followed')
